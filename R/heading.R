755f969b63b804ce2e372ec39401eee0a317abdb
Nick Davies <n.g.davies@gmail.com>
2025-09-06 22:50:57 +0100

    removing en dash from headings

--- R/heading.R
+++ R/heading.R
@@ -3,8 +3,8 @@
 #' Use a call to one of these functions as the `heading` parameter of
 #' [overlayServer()] to provide a heading on the overlay dropdown menu
 #' reporting the start and end position of the overlay. For numbers, the
-#' heading from [rangeHeading()] will be e.g. "1.5 – 3.5". For dates, the
-#' heading from [dateHeading()] will be e.g. "2025-05-01 – 2025-06-01".
+#' heading from [rangeHeading()] will be e.g. "1.5 - 3.5". For dates, the
+#' heading from [dateHeading()] will be e.g. "2025-05-01 - 2025-06-01".
 #'
 #' @param sep A separator that will be inserted between the start and end
 #'     position of the overlay. Use `NULL` to only print the start position.
@@ -24,7 +24,7 @@
 #' @seealso [overlayServer()], for a complete example.
 #'
 #' @export
-rangeHeading = function(..., sep = " – ") function(ov, i)
+rangeHeading = function(..., sep = " - ") function(ov, i)
 {
     cx = format(c(ov$cx0[i], ov$cx1[i]), ...)
     if (is.null(sep)) {
@@ -36,7 +36,7 @@ rangeHeading = function(..., sep = " – ") function(ov, i)
 
 #' @rdname rangeHeading
 #' @export
-dateHeading = function(format, ..., sep = " – ") function(ov, i)
+dateHeading = function(format, ..., sep = " - ") function(ov, i)
 {
     cx = as.Date(c(ov$cx0[i], ov$cx1[i]), origin = "1970-01-01")
     cx = format.Date(cx, format, ...)
